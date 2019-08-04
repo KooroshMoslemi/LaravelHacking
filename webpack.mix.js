6ac500f50b210fd11492185ad9725bc149f7c1d8
Koorosh <koorosh.moslemi91@gmail.com>
2019-08-04 11:34:32 +0430

    admin master page setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
-// mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/');
-mix.styles([
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .styles([
     'resources/css/libs/blog-post.css',
     'resources/css/libs/bootstrap.css',
     'resources/css/libs/font-awesome.css',
